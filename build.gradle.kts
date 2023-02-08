72bf4d05d83f61934a17f55340148213c3d15777
awumii <awumii@protonmail.com>
2023-02-08 19:20:38 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 allprojects {
-    group = "me.xneox"
+    group = "com.github.awumii"
     version = "1.0.1"
 }
 
@@ -28,13 +28,6 @@ subprojects {
 
     dependencies {
         implementation("org.jetbrains:annotations:24.0.0")
-
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    }
-
-    tasks.getByName<Test>("test") {
-        useJUnitPlatform()
     }
 
     tasks.withType<JavaCompile> {
